aeff8fe3fe24a28b1fc2a2d8d08be5f5ecc4e19e
Edwin Fenollal <efenollal@gmail.com~git config --global user.email efenollal@gmail.com>
2024-10-28 22:46:01 -0400

    uncomenting export in file to test issues with vercel/postgres

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -101,7 +101,7 @@
 //   return insertedRevenue;
 // }
 
-// export async function GET() {
+export async function GET() {
 //   return Response.json({
 //     message:
 //       'Uncomment this file and remove this line. You can delete this file when you are finished.',
@@ -119,4 +119,4 @@
 //     await client.sql`ROLLBACK`;
 //     return Response.json({ error }, { status: 500 });
 //   }
-// }
+}
